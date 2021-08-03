9797f94baa600db8fdce754ec6ee5697e0da23c9
Borewit <borewit@users.noreply.github.com>
2021-08-03 19:42:52 +0200

    Replace BrowserStack with XVFB

--- karma.conf.js
+++ karma.conf.js
@@ -66,45 +66,6 @@ module.exports = config => {
       combineBrowserReports: true // Combines coverage information from multiple browsers into one report
     },
 
-    // global BrowserStack configuration
-    browserStack: {
-      forcelocal: true,  // force traffic through the local BrowserStack tunnel, passes flag through to BrowserStackTunnel
-      project: 'music-metadata-browser',
-      timeout: 30,  // BROWSERSTACK_IDLE_TIMEOUT
-      captureTimeout: 30
-    },
-
-    // define browsers, see https://www.browserstack.com/automate/capabilities
-    customLaunchers: {
-      bs_win_chrome: {
-        base: 'BrowserStack',
-        os: 'Windows',
-        os_version: '10',
-        browser: 'Chrome',
-        browser_version: '88.0'
-      },
-      bs_win_firefox: {
-        base: 'BrowserStack',
-        os: 'Windows',
-        os_version: '10',
-        browser: 'Firefox',
-        browser_version: '84.0'
-      },
-      bs_osx_safari: {
-        base: 'BrowserStack',
-        os: 'OS X',
-        os_version: 'Big Sur',
-        browser: 'Safari',
-        browser_version: '14'
-      },
-      bs_win_edge: {
-        base: 'BrowserStack',
-        os: 'Windows',
-        os_version: '10',
-        browser: 'Edge',
-        browser_version: '88'
-      }
-    },
 
     mocha: {
       timeout: 20000 // 20 seconds
@@ -112,12 +73,6 @@ module.exports = config => {
 
     //autoWatch: true,
     browsers: ['Chrome'],
-    colors: true,
-
-    // Increase time-outs to prevent disconnects on BrowserStack
-    browserDisconnectTimeout: 10000, // default 2000
-    browserDisconnectTolerance: 1, // default 0
-    browserNoActivityTimeout: 4 * 60 * 1000, //default 10000
-    captureTimeout: 4 * 60 * 1000 //default 60000
+    colors: true
   });
 };
