a12812b6aeebb87eee0b1772e6dc28ba0378d115
Borewit <borewit@users.noreply.github.com>
2022-01-15 12:40:01 +0100

    Shim `blob.stream()` for Safari < 14.1
    Resolves Borewit/music-metadata-browser#624

--- lib/index.ts
+++ lib/index.ts
@@ -43,9 +43,43 @@ export async function parseBlob(blob: Blob, options?: IOptions): Promise<IAudioM
   if (blob instanceof File) {
     fileInfo.path = (blob as File).name;
   }
-  return parseReadableStream(blob.stream(), {mimeType: blob.type, size: blob.size}, options);
+
+  const stream = blob.stream ? blob.stream() : convertBlobToReadableStream(blob);
+  return parseReadableStream(stream, {mimeType: blob.type, size: blob.size}, options);
 }
 
+/**
+ * Convert Blob to ReadableStream
+ * Fallback for Safari versions < 14.1
+ * @param blob
+ */
+function convertBlobToReadableStream(blob: Blob): ReadableStream {
+
+  const fileReader = new FileReader();
+
+  return new ReadableStream({
+    start(controller) {
+      // The following function handles each data chunk
+      fileReader.onloadend = event => {
+        let data = (event.target as any).result;
+        if (data instanceof ArrayBuffer) {
+          data = new Uint8Array(data);
+        }
+        controller.enqueue(data);
+        controller.close();
+      };
+
+      fileReader.onerror = error => {
+        controller.close();
+      };
+
+      fileReader.onabort = error => {
+        controller.close();
+      };
+      fileReader.readAsArrayBuffer(blob);
+    }
+  });
+}
 /**
  * Parse fetched file, using the Web Fetch API
  * @param audioTrackUrl - URL to download the audio track from
